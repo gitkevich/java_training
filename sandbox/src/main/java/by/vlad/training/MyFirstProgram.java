22c47f1da9284399140694dce0cc5b8060daa315
gitkevich <vldpktvch@gmail.com>
2019-11-24 20:09:04 +0100

    Лекция 1.8. Классы и объекты

--- sandbox/src/main/java/by/vlad/training/MyFirstProgram.java
+++ sandbox/src/main/java/by/vlad/training/MyFirstProgram.java
@@ -1,5 +1,7 @@
 package by.vlad.training;
 
+import org.w3c.dom.css.Rect;
+
 public class MyFirstProgram {
 
     public static void main(String[] args) {
@@ -7,23 +9,25 @@ public class MyFirstProgram {
         hello("user");
         hello("Vlad");
 
-        double len = 5;
-        System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+        Square s = new Square(5);
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+        double len = 5;
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+
+        Rectangle r = new Rectangle(4,6);
+
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
     }
 
     public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody);
 	}
 
-	public static double area(double l){
-        return l+l;
+	public static double area(Square s){
+        return s.l * s.l;
     }
 
-    public static double area(double a, double b){
-        return a * b;
+    public static double area(Rectangle r){
+        return r.a * r.b;
     };
 }
---
+++ sandbox/src/main/java/by/vlad/training/Square.java
@@ -0,0 +1,10 @@
+package by.vlad.training;
+
+public class Square {
+
+    public double l;
+
+    public Square(double l){
+        this.l = l;
+    }
+}
