f7d0e461962b2bc5736176588bbf89f1280c8e2c
gitkevich <vldpktvch@gmail.com>
2019-11-25 22:54:55 +0100

    Выполнен пункт 4 из Задания №2

--- sandbox/src/main/java/by/vlad/training/DistanceCalculator.java
+++ sandbox/src/main/java/by/vlad/training/DistanceCalculator.java
@@ -7,15 +7,13 @@ public class DistanceCalculator {
     public static void main(String[] args) {
         Point p1 = new Point(5,3);
         Point p2 = new Point(2,7);
-        System.out.println("Расстояние между точкой с координатами ("+ p1.x + "," + p1.y + ") и точкой с координатами ("+ p2.x + "," + p2.y + ") равно " + distance(p1, p2));
+        System.out.println("Расстояние между точкой с координатами ("+ p1.x + "," + p1.y + ") и точкой с координатами ("+ p2.x + "," + p2.y + ") равно " + p1.distance(p2));
     }
 
     public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody);
 	}
 
-    public static double distance(Point p1, Point p2){
-        return Math.sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
-    }
+
 
 }
