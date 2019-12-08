be709409fc232fb3812ce70170ecea1b462b2750
gitkevich <vldpktvch@gmail.com>
2019-12-08 14:28:30 +0100

    Создан тест для модификации группы

---
+++ addressbook-web-tests/src/test/java/by/vlad/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,16 @@
+package by.vlad.addressbook.tests;
+
+import by.vlad.addressbook.model.GroupData;
+import org.testng.annotations.Test;
+
+public class GroupModificationTests extends TestBase {
+    @Test
+    public void testGroupModification(){
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("Test1", "Test2", "Test3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
