889917f3c98e2cafcfa5779d2f79d9c9fc70aa26
gitkevich <vldpktvch@gmail.com>
2019-12-09 21:33:47 +0100

    Реализовать возможность запуска тестов в браузерах Chrome и Firefox

--- addressbook-web-tests/src/test/java/by/vlad/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/by/vlad/addressbook/tests/TestBase.java
@@ -1,14 +1,13 @@
 package by.vlad.addressbook.tests;
 
 import by.vlad.addressbook.appmanager.ApplicationManager;
-import by.vlad.addressbook.model.ContactData;
-import org.openqa.selenium.By;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
