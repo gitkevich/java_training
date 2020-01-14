d590ae35e720c95dd2dcb7fc5700f1bd97f2a6f8
gitkevich <vldpktvch@gmail.com>
2020-01-14 21:25:20 +0100

    Рефакторинг тестов для групп (лекция 5.2)

--- addressbook-web-tests/src/test/java/by/vlad/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/by/vlad/addressbook/tests/GroupDeletionTests.java
@@ -2,19 +2,23 @@ package by.vlad.addressbook.tests;
 
 import by.vlad.addressbook.model.GroupData;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class GroupDeletionTests extends TestBase{
 
-    @Test
-    public void testGroupDeletion() throws Exception {
+    @BeforeMethod
+    public void ensurePreconditions(){
         app.getNavigationHelper().gotoGroupPage();
-
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("Test1", null, null));
         }
+    }
+
+    @Test
+    public void testGroupDeletion() throws Exception {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().deleteSelectedGroups();
