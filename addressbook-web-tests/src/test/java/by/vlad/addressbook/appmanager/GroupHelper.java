a6f2801453754db52b93e42055687209046bdec0
gitkevich <vldpktvch@gmail.com>
2020-01-22 22:35:09 +0100

    лекция 5.5

--- addressbook-web-tests/src/test/java/by/vlad/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/by/vlad/addressbook/appmanager/GroupHelper.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.WebElement;
 
 import java.security.acl.Group;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GroupHelper extends HelperBase {
 
@@ -41,6 +43,11 @@ public class GroupHelper extends HelperBase {
         wd.findElements(By.name("selected[]")).get(index).click();
     }
 
+    public void selectGroupById(int id) {
+        wd.findElement(By.cssSelector("input[name='" + id + "']")).click();
+    }
+
+
     public void initGroupModification() {
         click(By.name("edit"));
     }
@@ -70,6 +77,12 @@ public class GroupHelper extends HelperBase {
         returnToGroupPage();
     }
 
+    public void delete(GroupData group) {
+        selectGroupById(group.getId());
+        deleteSelectedGroups();
+        returnToGroupPage();
+    }
+
     public boolean isThereAGroup() {
         return isElementPresent(By.name("selected[]"));
     }
@@ -80,6 +93,17 @@ public class GroupHelper extends HelperBase {
 
     public List<GroupData> list() {
         List<GroupData> groups = new ArrayList<GroupData>();
+        List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+        for (WebElement element : elements){
+            String name = element.getText();
+            int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
+            groups.add(new GroupData().withId(id).withName(name));
+        }
+        return groups;
+    }
+
+    public Set<GroupData> all() {
+        Set<GroupData> groups = new HashSet<GroupData>();
         List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
         for (WebElement element : elements){
             String name = element.getText();
--- addressbook-web-tests/src/test/java/by/vlad/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/by/vlad/addressbook/model/GroupData.java
@@ -8,6 +8,24 @@ public class GroupData {
     private String header;
     private String footer;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        if (id != groupData.id) return false;
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
     public GroupData withName(String name) {
         this.name = name;
         return this;
@@ -32,21 +50,6 @@ public class GroupData {
         return id;
     }
 
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GroupData groupData = (GroupData) o;
-
-        return name != null ? name.equals(groupData.name) : groupData.name == null;
-    }
-
-    @Override
-    public int hashCode() {
-        return name != null ? name.hashCode() : 0;
-    }
 
     public String getName() {
         return name;
